78884116294b7f9c3c52cdd9201e87d8309c6070
n <2585803+tt5@users.noreply.github.com>
2024-01-06 16:21:03 +0100

    protect sql input

--- routes/db/autohooks.js
+++ routes/db/autohooks.js
@@ -1,15 +1,16 @@
 'use strict'
 const fp = require('fastify-plugin')
 const schemas = require('./schemas/loader')
+const { sql } = require('squid/pg')
 
 module.exports = fp(async function dbAutoHooks (fastify, opts) {
 
   fastify.register(schemas)
 
   fastify.decorate('postgresDataSource', {
-    async queryDb (sql) {
+    async queryDb (q) {
       const db = await fastify.pg.connect()
-      const { rows } = await db.query(sql)
+      const { rows } = await db.query(sql`${q}`)
       db.release()
       return rows
     },
