0dd4f40fa6f6a16c664431b671c4e4ec70431c90
n <2585803+tt5@users.noreply.github.com>
2024-02-29 11:35:57 +0100

    auth

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,32 @@
 'use strict'
 
+import oauthPlugin from '@fastify/oauth2'
+
 module.exports = async function root (fastify, opts) {
+
+  fastify.register(oauthPlugin, {
+    name: 'githubOAuth2',
+    credentials: {
+      client: {
+        id: process.env.GITHUB_ID,
+        secret: process.env.GITHUB_SECRET
+      },
+      auth: oauthPlugin.GITHUB_CONFIGURATION
+    },
+    startRedirectPath: '/login/github',
+    callbackUri: 'http://localhost:3000/login/github/callback'
+  })
+
   fastify.get('/', async function welcomeHandler (request, reply) {
     return { root: true }
   })
+
+  fastify.get('/login/github/callback', async (req, rep) => {
+
+    const token = await fastify.githubOAuth2.getAccessTokenFromAuthorizationCodeFlow(req)
+
+    return {hello: token}
+  })
 }
+
+
